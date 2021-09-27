474fb7440a753aa811488e09cf4271deddc92482
Kadeem140 <Beckford.kadeem@yahoo.com>
2021-09-26 20:03:35 -0400

    Completed Assignments, all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "Deem" ,
+    streetAddess: "123 Fake drive"
+}
+
+//making a method to update employee's key and value
+//What does it need? object, key and value arguments?
+//non destructively
+function updateEmployeeWithKeyAndValue(object, key, value){
+    const obj = {...object} //undestructive copy.
+    obj[key] = value
+    return obj
+}
+
+//destructively 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value
+    return object
+}
+
+function deleteFromEmployeeByKey(object, key){
+    const obj = {...object}
+    delete obj[key]
+    return obj
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key){
+    delete object[key]
+    return object
+}
